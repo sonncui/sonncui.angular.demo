55a5ba511e40c15b41fff7e3cc4d19aec2b79c06
cuicui <ccsong@CNccsong.local>
2015-01-21 23:48:52 +0800

    add testing

---
+++ testing/List.js
@@ -0,0 +1,33 @@
+/**
+ * Created by ccsong on 1/14/15.
+ */
+"use strict";
+angular.module('myList',['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl:'pages/firstPage.html',
+                controller:'ListController'
+            })
+            .when('/myfood', {
+                templateUrl:'pages/myfood.html',
+                controller:'MyFoodController'
+            })
+    }])
+    .controller('ListController',['$scope','$locate',function($scope,$locate){
+        $scope.items = [{text:"chicken",selected:false},{text:"beaf",selected:false},{text:"veg",selected:false}];
+
+        $scope.addItem = function(){
+            $scope.items.push({text:$scope.addText,selected:false});
+            $scope.addText = '';
+        };
+
+        $scope.select = function(){
+            for(var item in $scope.items){
+                if($scope.items[item].selected == true){
+                    $scope.myFood = $scope.items[item].text;
+                }
+            }
+            $locate.path('/myfood');
+        };
+    }]);
---
+++ testing/scripts/MyFood.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ccsong on 1/16/15.
+ */
+'use strict';
+angular.module('myList',[])
+.controller('MyFoodController',['$scope','$rootScope',function($scope,$rootScope){}]);
